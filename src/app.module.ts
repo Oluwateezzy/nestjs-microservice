ecdc81ec137167a38454c1e0bfea43a5bf3a2cb0
Oluwatobiloba <oluwateezzy03@gmail.com>
2024-06-26 11:56:04 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MathModule } from './math/math.module';
+import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
+  imports: [MathModule, UserModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
